99badb02f1b22692ccea244327f041413a048e96
RodrigoSantosDev82 <166881134+RodrigoSantosDev82@users.noreply.github.com>
2025-03-11 18:22:28 -0300

    step2_template

--- gestao/settings.py
+++ gestao/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-p
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
